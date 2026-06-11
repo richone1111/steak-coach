50a63e5ed04571f87e48ce264f6a38d2a9f616b0
richone1111 <minamimiminamimimi@gmail.com>
2026-06-11 15:57:02 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,15 @@
 const CACHE_NAME = 'bancho-steak-v1';
 
-// 在这里，把联网的像素包网址也加进去
+// 这里已经把你联网的像素字体包完美加入缓存列表
 const ASSETS = [
   './',
   './index.html',
   './dave-ui-theme.css',
   './manifest.json',
-  'https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap' // 👈 举例：换成你代码里实际联网的那个网址！
+  'https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap'
 ];
 
-// 安装时缓存所有资源（包括那个外链网址）
+// 安装时缓存所有资源（包括谷歌像素字体）
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
